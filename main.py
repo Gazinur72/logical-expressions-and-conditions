c7bce6e380afb95c7567e513b7d9515685152d8f
Gaz <karimovgaz72@gmail.com>
2025-03-23 16:13:50 +0500

    Убрал азкоментированую строку, и лишние пустые строки.

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ def choose_yes_no(question):
             return response
         print("Пожалуйста, выберите 'Да' или 'Нет'.")
 
+
 age = choose_yes_no("Вы достигли совершенолетия? Напишите: Да или Нет ").capitalize()
-    # (int(input("Введите ваш возраст: ")))
 
 citizenship = choose_yes_no("\nЯвляетесь ли вы гражданином РФ? Напишите: Да или Нет ").capitalize()
 
-
 limitations = choose_yes_no("\nЕсть ли у вас судимости либо другие ограничения для участия "
                             "в голосовании на выборах? Напишите: Да или Нет ").capitalize()
-
 
 if age == "Да" and citizenship == "Да" and limitations == "Нет":
     print("\nВы можете участвовать в голосовании.")
