06fca2faf3c406dfb0ea9cdf72abeab925159bf5
Gaz <karimovgaz72@gmail.com>
2025-03-23 16:11:37 +0500

    Переписал вопрос с возростам так что бы можно было использовать функцию выбора да или нет, убрал лишние вывод инфолрмации о том что выбрал полльзователь.

--- main.py
+++ main.py
@@ -5,22 +5,17 @@ def choose_yes_no(question):
             return response
         print("Пожалуйста, выберите 'Да' или 'Нет'.")
 
-age = int(input("Введите ваш возраст: "))
+age = choose_yes_no("Вы достигли совершенолетия? Напишите: Да или Нет ").capitalize()
+    # (int(input("Введите ваш возраст: ")))
 
 citizenship = choose_yes_no("\nЯвляетесь ли вы гражданином РФ? Напишите: Да или Нет ").capitalize()
-if citizenship == "Да":
-    print("Вы выбрали 'Да'\n")
-else:
-    print("Вы выбрали 'Нет'\n")
+
 
 limitations = choose_yes_no("\nЕсть ли у вас судимости либо другие ограничения для участия "
                             "в голосовании на выборах? Напишите: Да или Нет ").capitalize()
-if limitations == "Да":
-    print("Вы выбрали 'Да'")
-else:
-    print("Вы выбрали 'Нет'")
 
-if age >= 18 and citizenship == "Да" and limitations == "Нет":
+
+if age == "Да" and citizenship == "Да" and limitations == "Нет":
     print("\nВы можете участвовать в голосовании.")
 else:
     print("\nВы не можете участвовать в голосовании.")
